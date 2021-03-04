c4dfa63d719b44480e18c9a585e90834e9e9d59d
manabapp <manabapp>
2021-03-04 16:29:08 +0900

    Initial Commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Hirose Manabu on 2021/03/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
